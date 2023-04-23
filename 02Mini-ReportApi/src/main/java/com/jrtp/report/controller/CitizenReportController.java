8396c810e318415cc146b782dc66c85367847c13
arman71275 <armani71275@gmail.com>
2023-04-23 08:56:46 +0530

    added dynamic search controller for filtering citizen

--- 02Mini-ReportApi/src/main/java/com/jrtp/report/controller/CitizenReportController.java
+++ 02Mini-ReportApi/src/main/java/com/jrtp/report/controller/CitizenReportController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,6 +37,13 @@ public class CitizenReportController {
 		
 		
 	}
+	
+	@PostMapping("/search")
+	public ResponseEntity<List<CitizenPlan>> search(@RequestBody SearchRequest request) {
+		List<CitizenPlan> citizenPlans = reportService.getCitizenPlans(request);
+		return new ResponseEntity<>(citizenPlans, HttpStatus.OK);
+	}
+	
 	@GetMapping("/excel")
 	public void generateExcelReport(HttpServletResponse response) throws Exception{
 		
