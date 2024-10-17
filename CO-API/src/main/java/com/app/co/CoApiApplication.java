c8beba6b768837054102ff61744e199dab3a20c0
arman71275 <armani71275@gmail.com>
2024-10-17 20:06:00 +0530

    "first commit in Correspondence api"

---
+++ CO-API/src/main/java/com/app/co/CoApiApplication.java
@@ -0,0 +1,14 @@
+package com.app.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoApiApplication.class, args);
+		System.out.println("hi ");
+	}
+
+}
---
+++ CO-API/src/test/java/com/app/co/CoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
